f4ed3678c3b66469d16fcd951688b9d11abb064d
wtbhk <wtbhk@live.cn>
2014-12-28 13:10:13 +0800

    seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,7 +9,25 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Eloquent::unguard();
+                $user = User::create(array(
+                        'phone'=>12345678909,
+                        'nickname'=>'testnickname',
+                        'name'=>'testname',
+                        'wechat_id'=>'testopenid',
+                        'balance'=>121.5
+                ));
+                for($i=0,$i<20,$i++)
+                {
+                        Product::create(array(
+                                'price'=>20,
+                                'reservation_day'=>3,
+                                'inventory_per_day'=>20,
+                                'title'=>'testproduct'.$i,
+                                'description'=>'testdescription'.$i,
+                                'content'=>'testcontent'.$i,
+                                'rank'=>$i
+                        ));
+                }
 
 		// $this->call('UserTableSeeder');
 	}
