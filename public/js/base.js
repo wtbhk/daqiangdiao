e50085e5b4c365937bb5c1d8456bad03348f778e
zvenshy <zvenshy@gmail.com>
2014-12-31 20:38:27 +0800

    ...

--- public/js/base.js
+++ public/js/base.js
@@ -89,11 +89,13 @@ $(document).ready(function () {
         $('#time').change(function () {
             $('input[name="time"]').val($(this).val());
             $('input[name="today"]').val('false');
-            $('form').eq(0).submit();
         });
         $('#now').click(function () {
             $('input[name="time"]').val('');
             $('input[name="today"]').val('true');
+            $('form').eq(0).submit();
+        });
+        $('body').on('click', 'button.ui-datepicker-close', function () {
             $('form').eq(0).submit();
         })
     }());
