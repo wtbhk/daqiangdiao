95bbbacd0f2d196506878310523b88ac53f95ca3
wtbhk <wtbhk@live.cn>
2015-04-21 20:21:50 +0800

    查看购物车

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,6 +1,6 @@
     <footer class="foot-fixed">
         <a class="wrap" href="/cart">
             <span class="get fl"></span>
-            <span class="delivery fr">￥<?php echo $minimum_amount; ?>查看购物车</span>
+            <span class="delivery fr">查看购物车</span>
         </a>
     </footer>
