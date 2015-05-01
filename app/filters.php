a8b0d78741d4c838cd1158fb9a04e30091fcad36
wtbhk <wtbhk@live.cn>
2015-05-01 10:21:28 +0800

    修改KEY

--- app/filters.php
+++ app/filters.php
@@ -14,7 +14,7 @@
 App::before(function($request)
 {
         Config::set('wechat.appid', 'wxc2b448d7bf4f74a6');
-        Config::set('wechat.secret', 'ad4483be94fc0e204ca59a726c02a45a'); 
+        Config::set('wechat.secret', 'db8ae5dcdef7d20cbe482cc2f5b3f961'); 
         View::share('minimum_amount', Setting::get('minimum_amount', 50));
 });
 
