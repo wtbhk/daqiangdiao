62c1b86e6466371f1988bbe5e04cd7423fbb693d
wtbhk <wtbhk@live.cn>
2015-05-16 15:20:11 +0800

    fix

--- app/controllers/WechatController.php
+++ app/controllers/WechatController.php
@@ -15,16 +15,15 @@ class WechatController extends BaseController {
                 if(Session::has('wechat_userinfo'))
                 {
                         $wechat_user_info = Session::get('wechat_userinfo');
+                        $subscribe = ($wechat_user_info['subscribe']==0) ? false : true;
+                        $this->user->subscribe = $subscribe;
                         $this->wechat_user_info = $wechat_user_info;
                         $userinfo = Userinfo::find($wechat_user_info['openid']);
                         if(!$userinfo) {
-                                $wechat_user_info['subscribe'] = ($wechat_user_info['subscribe']==0) ? false : true;
                                 unset($wechat_user_info['remark']);
                                 unset($wechat_user_info['groupid']);
+                                unset($wechat_user_info['subscribe']);
                                 $userinfo = Userinfo::create($wechat_user_info);
-                                $userinfo->save();
-                        }else{
-                                $userinfo->subscribe = ($wechat_user_info['subscribe']==0) ? false : true;
                                 $userinfo->save();
                         }
                 }
