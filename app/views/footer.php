6b4730194bf4c680375479b0bb3a90ec15517e94
zvenshy <zvenshy@gmail.com>
2015-05-21 21:17:23 +0800

    footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,6 +1,9 @@
     <footer class="foot-fixed">
-        <a class="wrap" href="/cart">
-            <span class="get fl"></span>
-            <span class="delivery fr">查看购物车</span>
-        </a>
+        <a class="delivery" href="/cart"></a>
+        <ul>
+            <li class="profile"><a href="/profile">我的</a></li>
+            <li><a href="/">首页</a></li>
+            <li><a href="/chef">封厨榜</a></li>
+            <li><a href="/list">饕餮盛宴</a></li>
+        </ul>
     </footer>
