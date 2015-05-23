7fedaec488f4aa60a0b14a602e81588510d25681
zvenshy <zvenshy@gmail.com>
2015-05-23 19:54:59 +0800

    cheflist.php

---
+++ app/views/cheflist.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="keywords" content="rcss">
+    <meta name="description" content="大腔调">
+    <meta name="author" content="zvenshy@gmail.com">
+    <title>购物车 - 大腔调</title>
+    <link rel="stylesheet" href="/css/cheflist.css">
+
+</head>
+<body>
+    <div id="content">
+        <div id="cheflist">
+            <ul>
+                <li>
+                    <div class="avatar"><img src="images/head.jpg" alt=""></div>
+                    <div class="rank"><img src="images/rank.jpg" alt=""></div>
+                    <div class="header">
+                        <span class="name">厨师艺名</span>
+                        <p class="profile">我的地盘我做主</p>
+                    </div>
+                    <div class="imglist">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                    </div>
+                </li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    <footer class="foot-fixed">
+        <a class="delivery" href="/cart"></a>
+        <ul>
+            <li class="profile"><a href="/profile">我的</a></li>
+            <li><a href="/">首页</a></li>
+            <li><a href="/chef">封厨榜</a></li>
+            <li class="active"><a href="/list">饕餮盛宴</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
