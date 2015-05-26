71d28eacc605e031c65ba7584306659154008eea
zvenshy <zvenshy@gmail.com>
2015-05-26 19:56:15 +0800

    footer 使用font-icon

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,7 +1,7 @@
     <footer class="foot-fixed">
         <a class="delivery" href="/cart"></a>
         <ul>
-            <li class="profile"><a href="/profile">我的</a></li>
+            <li class="profile"><a href="/profile"><i class="fa fa-user fa-lg"></i></a></li>
             <li><a href="/">腔调首推</a></li>
             <li><a href="/cheflist">封厨榜单</a></li>
             <li><a href="/list">饕餮点单</a></li>
